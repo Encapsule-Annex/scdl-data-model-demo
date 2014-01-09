d67f0e8146c0aff056d185cda71e58f8a820e21f
Chris Russell <chrisrus@encapsule.org>
2014-01-08 18:39:08 -0800

    scdl-data-model-demo v0.0.1

---
+++ index.js
@@ -0,0 +1,38 @@
+// scdl-data-model-demo/index.js
+// MIT by ChrisRus
+
+// onm allows us to share data models via npm.
+var onm = require('onm');
+
+// onmd-scdl is an onm data model for something called SCDL.
+var scdl = require('onmd-scdl');
+
+// create an onm data model object for SCDL.
+var scdlModel = new onm.Model(scdl.DataModel);
+
+// create an onm data store object for SCDL.
+var scdlStore = new onm.Store(scdlModel);
+
+// Given the address of the root of an onm data component namespace,
+// visit all of the component's extension point subnamespaces, create
+// a new subcomponent instance in each extension point, and invoke this
+// function recursively until there are no more extension points OR
+// height_ >= limit_.
+//
+var createSubcomponents = function (address_, limit_, height_) {
+    if (limit_ > height_) {
+        address_.visitExtensionPointAddresses( function (address_) {
+            var newComponentNamespace = scdlStore.createComponent(address_.createSubcomponentAddress());
+            var newComponentAddress = newComponentNamespace.getResolvedAddress();
+            console.log("+component: '" + newComponentAddress.getHashString() +"'");
+            createSubcomponents(newComponentAddress, limit_, height_ + 1);
+	});
+    }
+};
+
+// Leverage onm data model introspection to "explode" the data
+// component tree in scdlStore. See comments above.
+createSubcomponents(scdlModel.createRootAddress(), 256/*limit*/, 0/*initial height*/);
+
+// Serialize the test data to JSON and write to console.log.
+console.log(scdlStore.toJSON(undefined, 1));
